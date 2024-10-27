3f665982f48768d699403636963f70dc5cb8cd46
Silpa <silpa.jr@gmail.com>
2024-10-27 14:09:10 +0800

    kubernetes

--- eksctl-resources/k8-install.sh
+++ eksctl-resources/k8-install.sh
@@ -17,3 +17,4 @@ curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linu
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl.sha256
 sha256sum -c kubectl.sha256
 chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
